df0632ac96db8068d63a543f15683aa6ad867154
sh414lim <77113306+sh414lim@users.noreply.github.com>
2023-02-02 14:35:57 +0900

    update : ui 수정

--- korea_front/.next/static/chunks/webpack.js
+++ korea_front/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "30b2730e02f71715"; }
+/******/ 		__webpack_require__.h = function() { return "f08d232224bb5012"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- korea_front/pages/Intro.tsx
+++ korea_front/pages/Intro.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import Login from "../layout/Login";
+import Introduce from "./main/Introduce";
 
 export default function Intro() {
   return (
     <>
-      <Login />
+      <Introduce />
+      {/* <Login /> */}
     </>
   );
 }
