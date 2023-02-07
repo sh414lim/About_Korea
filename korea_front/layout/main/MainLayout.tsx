3abb66b712874ec6fe17915b58fc4e86c9e693c0
sh414lim <77113306+sh414lim@users.noreply.github.com>
2023-02-07 15:26:43 +0900

    feat: 리팩토링(네이밍 폴더구조)_

---
+++ korea_front/layout/main/MainLayout.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Header from "../Header";
+import styled from "styled-components";
+
+type Props = {};
+
+const Container = styled.div`
+  margin: 0 auto;
+`;
+const BannerList = styled.div`
+  width: 100%;
+  height: 45vh;
+  background: #ddd;
+`;
+const MainContainer = styled.div`
+  max-width: 85%;
+  width: 100%;
+  margin: 2rem auto;
+  &.flex {
+    display: flex;
+    gap: 2%;
+  }
+`;
+const MainContent = styled.div`
+  width: 100%;
+  &.half {
+    width: 49%;
+  }
+`;
+const DumeContent = styled.div`
+  height: 30vh;
+  border-radius: 5px;
+  box-shadow: 3px 5px 10px #00000026;
+  padding: 0.5rem;
+`;
+
+export default function MainLayout({ children }: any) {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+    </>
+  );
+}
